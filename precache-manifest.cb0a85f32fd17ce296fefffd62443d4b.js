1c0ec81558f4aeb7208e92cf80f24589ff0a3d00
ng <nadavgehasi@gmail.com>
2020-03-22 20:45:48 +0200

    Updates

---
+++ precache-manifest.cb0a85f32fd17ce296fefffd62443d4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779042179b826abc9af57abaa2e896ba",
+    "url": "/system-viewer/index.html"
+  },
+  {
+    "revision": "11bf5c45ff08c2b01cf9",
+    "url": "/system-viewer/static/css/2.46a2d066.chunk.css"
+  },
+  {
+    "revision": "87b41d69a685422843ab",
+    "url": "/system-viewer/static/css/main.76f98959.chunk.css"
+  },
+  {
+    "revision": "11bf5c45ff08c2b01cf9",
+    "url": "/system-viewer/static/js/2.daf42e96.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/system-viewer/static/js/2.daf42e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b41d69a685422843ab",
+    "url": "/system-viewer/static/js/main.3b62e116.chunk.js"
+  },
+  {
+    "revision": "cabc35f7bcd2acca7255",
+    "url": "/system-viewer/static/js/runtime-main.9e4ce56a.js"
+  }
+]);

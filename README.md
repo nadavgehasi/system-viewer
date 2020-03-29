bb217c24a06932d411593e0ba5faf362e5e98b47
Ophir Taieb <ophir@localhost.localdomain>
2020-03-29 12:50:22 +0300

    Initial Django Backend

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # system-viewer
+
+### Backend
+
+Deployed on AWS
+http://dockerweb-env.eba-pbxve5yu.us-east-1.elasticbeanstalk.com/
+
+Technologies used:
+- Python 3.8
+- Django 3.0
+- Django Rest framework
+- Docker
+- Docker-compose
+- Poetry
+- Pyenv
+- AWS Elastic Beanstalk
---
+++ backend/pages/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework import viewsets
+
+from .serializers import ServerSerializer
+from .models import Server
+from rest_framework import permissions
+
+
+def home_page_view(request):
+    return HttpResponse('Hello, World!')
+
+
+class ServerViewSet(viewsets.ModelViewSet):
+    queryset = Server.objects.all().order_by('name')
+    serializer_class = ServerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]

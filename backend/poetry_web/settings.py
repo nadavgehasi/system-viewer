e0b54e1f20436cb1068f2f81ba87789360455c9e
Ophir Taieb <ophir@localhost.localdomain>
2020-03-30 16:51:43 +0300

    Add server to settings

--- backend/poetry_web/settings.py
+++ backend/poetry_web/settings.py
@@ -24,9 +24,8 @@ SECRET_KEY = '5=g%9hbff0+$h*sg9eh1v#=q)wy%&#2adejph0izz(y==&60uq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dockerweb-env.eba-pbxve5yu.us-east-1.elasticbeanstalk.com', '192.168.0.104',
-                 'dockerweb-dev.us-east-1.elasticbeanstalk.com', 'localhost', '127.0.0.1',
-                 'system-viewer.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['192.168.0.104', 'localhost', '127.0.0.1', 'system-viewer.us-east-1.elasticbeanstalk.com',
+                 'system-viewer-dev.us-east-1.elasticbeanstalk.com']
 
 # Application definition
 

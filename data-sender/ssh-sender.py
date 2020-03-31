10e7c6c6cac2afd6e21b8622f9947f98978db7dc
Ophir Taieb <ophir@localhost.localdomain>
2020-04-01 01:32:03 +0300

    monitor localhost

--- data-sender/ssh-sender.py
+++ data-sender/ssh-sender.py
@@ -18,6 +18,7 @@ HOSTS = [
     (('172.17.0.1', 'armon-yarkon-2'), 'root', 'root'),
     (('192.168.0.104', 'badid-mif'), 'root', 'root'),
     (('172.17.0.1', 'docker_host'), 'root', 'root'),
+    (('172.17.0.1', 'localhost'), 'root', 'root'),
 ]
 
 

7f083ad4e989f210ef0677be523949b24596f823
ng <nadavgehasi@gmail.com>
2020-04-01 12:55:11 +0300

    Rename teamList to teams

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import AppMenu from "./components/Menu/AppMenu";
 import ServersTable from "./components/ServersTable/ServersTable";
-import TeamsList from "./components/TeamsList/TeamsList";
+import TeamsList from "./components/Teams/TeamsList";
 import SystemsList from "./components/Systems/SystemsList";
 import "./App.css";
 
